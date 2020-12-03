bc4836e4b6acdd1af725be529b467daaf39b3f60
dtechdevelopers07 <55192607+dtechdevelopers07@users.noreply.github.com>
2020-12-03 21:53:10 +0500

    Update index.sh

--- index.sh
+++ index.sh
@@ -3,15 +3,15 @@
 client="client";
 newclient () {
 	# Generates the custom client.ovpn
-	cp /etc/openvpn/client-common.txt /etc/openvpn/clients/$1.ovpn
+	cp /etc/openvpn/client-common.txt /etc/openvpn/client/$1.ovpn
 	echo "<ca>" >> /etc/openvpn/clients/$1.ovpn
-	cat /etc/openvpn/easy-rsa/pki/ca.crt >> /etc/openvpn/clients/$1.ovpn
+	cat /etc/openvpn/easy-rsa/pki/ca.crt >> /etc/openvpn/client/$1.ovpn
 	echo "</ca>" >> /etc/openvpn/clients/$1.ovpn
 	echo "<cert>" >> /etc/openvpn/clients/$1.ovpn
-	cat /etc/openvpn/easy-rsa/pki/issued/$1.crt >> /etc/openvpn/clients/$1.ovpn
+	cat /etc/openvpn/easy-rsa/pki/issued/$1.crt >> /etc/openvpn/client/$1.ovpn
 	echo "</cert>" >> /etc/openvpn/clients/$1.ovpn
 	echo "<key>" >> /etc/openvpn/clients/$1.ovpn
-	cat /etc/openvpn/easy-rsa/pki/private/$1.key >> /etc/openvpn/clients/$1.ovpn
+	cat /etc/openvpn/easy-rsa/pki/private/$1.key >> /etc/openvpn/client/$1.ovpn
 	echo "</key>" >> /etc/openvpn/clients/$1.ovpn
 	echo "<tls-auth>" >> /etc/openvpn/clients/$1.ovpn
 	cat /etc/openvpn/ta.key >> /etc/openvpn/clients/$1.ovpn
@@ -38,5 +38,5 @@ newclient "$client"
 # echo ""
 while read c; do
 	echo $c
-done </etc/openvpn/clients/$client.ovpn
+done </etc/openvpn/client/$client.ovpn
 exit 0
