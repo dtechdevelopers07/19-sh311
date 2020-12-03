eb6a5270778bda97c95ca64cf7d464a676079337
dtechdevelopers07 <55192607+dtechdevelopers07@users.noreply.github.com>
2020-12-03 22:00:08 +0500

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 # client="$(curl ifconfig.me)"
 client="client";
-newclient () {
+new_client () {
 	# Generates the custom client.ovpn
-	cp /etc/openvpn/client-common.txt /etc/openvpn/client/$1.ovpn
-	echo "<ca>" >> /etc/openvpn/clients/$1.ovpn
-	cat /etc/openvpn/easy-rsa/pki/ca.crt >> /etc/openvpn/client/$1.ovpn
-	echo "</ca>" >> /etc/openvpn/clients/$1.ovpn
-	echo "<cert>" >> /etc/openvpn/clients/$1.ovpn
-	cat /etc/openvpn/easy-rsa/pki/issued/$1.crt >> /etc/openvpn/client/$1.ovpn
-	echo "</cert>" >> /etc/openvpn/clients/$1.ovpn
-	echo "<key>" >> /etc/openvpn/clients/$1.ovpn
-	cat /etc/openvpn/easy-rsa/pki/private/$1.key >> /etc/openvpn/client/$1.ovpn
-	echo "</key>" >> /etc/openvpn/clients/$1.ovpn
-	echo "<tls-auth>" >> /etc/openvpn/clients/$1.ovpn
-	cat /etc/openvpn/ta.key >> /etc/openvpn/clients/$1.ovpn
-	echo "</tls-auth>" >> /etc/openvpn/clients/$1.ovpn
+	{
+	cat /etc/openvpn/server/client-common.txt
+	echo "<ca>"
+	cat /etc/openvpn/server/easy-rsa/pki/ca.crt
+	echo "</ca>"
+	echo "<cert>"
+	sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/"$1".crt
+	echo "</cert>"
+	echo "<key>"
+	cat /etc/openvpn/server/easy-rsa/pki/private/"$1".key
+	echo "</key>"
+#	echo "<tls-crypt>"
+#	sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
+#	echo "</tls-crypt>"
+	} > /etc/openvpn/client/"$1".ovpn
 }
-cd /etc/openvpn/easy-rsa/
+cd /etc/openvpn/server/easy-rsa/
 
  #Revoke a client
 ./easyrsa --batch revoke $client
@@ -25,8 +27,8 @@ cd /etc/openvpn/easy-rsa/
 rm -rf pki/reqs/$client.req
 rm -rf pki/private/$client.key
 rm -rf pki/issued/$client.crt
-rm -rf /etc/openvpn/crl.pem
-cp /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn/crl.pem
+rm -rf /etc/openvpn/server/crl.pem
+cp /etc/openvpn/server/easy-rsa/pki/crl.pem /etc/openvpn/server/crl.pem
 
 #Add a client
 ./easyrsa build-client-full $client nopass
