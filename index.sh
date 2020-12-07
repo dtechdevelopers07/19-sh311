753367d4e8cdb00390fab6ae3c94b7e1d4857d07
dtechdevelopers07 <55192607+dtechdevelopers07@users.noreply.github.com>
2020-12-07 05:24:30 +0500

    Update index.sh

--- index.sh
+++ index.sh
@@ -4,22 +4,22 @@ client="client";
 new_client () {
 	# Generates the custom client.ovpn
 	{
-	cat /etc/openvpn/server/client-common.txt
+	cat /etc/openvpn/client-common.txt
 	echo "<ca>"
-	cat /etc/openvpn/server/easy-rsa/pki/ca.crt
+	cat /etc/openvpn/easy-rsa/pki/ca.crt
 	echo "</ca>"
 	echo "<cert>"
-	sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/"$1".crt
+	sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/easy-rsa/pki/issued/"$1".crt
 	echo "</cert>"
 	echo "<key>"
-	cat /etc/openvpn/server/easy-rsa/pki/private/"$1".key
+	cat /etc/openvpn/easy-rsa/pki/private/"$1".key
 	echo "</key>"
 #	echo "<tls-crypt>"
 #	sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
 #	echo "</tls-crypt>"
 	} > /etc/openvpn/client/"$1".ovpn
 }
-cd /etc/openvpn/server/easy-rsa/
+cd /etc/openvpn/easy-rsa/
 
  #Revoke a client
 ./easyrsa --batch revoke $client
@@ -28,16 +28,16 @@ rm -rf pki/reqs/$client.req
 rm -rf pki/private/$client.key
 rm -rf pki/issued/$client.crt
 rm -rf /etc/openvpn/server/crl.pem
-cp /etc/openvpn/server/easy-rsa/pki/crl.pem /etc/openvpn/server/crl.pem
+cp /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn/crl.pem
 
 #Add a client
 ./easyrsa build-client-full $client nopass
 # Generates the custom client.ovpn
 new_client "$client"
 
-# echo "Content-type: text/file"
+echo "Content-type: text/file"
 # echo "Content-Disposition: attachment; filename=\"$client.ovpn\""
-# echo ""
+echo ""
 while read c; do
 	echo $c
 done </etc/openvpn/client/$client.ovpn
